bb8cf14468b222a5c4d344b2cce8041ef7aa2484
bijan <bmassoumi@gmail.com>
2024-10-20 13:40:28 -0400

    change show link

--- src/utils.ts/fetchShows.ts
+++ src/utils.ts/fetchShows.ts
@@ -4,13 +4,14 @@ import Papa from "papaparse";
 export const fetchShows = async (): Promise<Show[]> => {
   try {
     const response = await fetch(
-      "https://docs.google.com/spreadsheets/d/1Ng4Rb2fhZETYA5BYg2Pjr5Zqc2FgGonxP7tvHJKz5aA/pub?output=csv"
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vRcnjsq9ckno-2onPEQsqqc86DtkWEwI8dP3rKk50SkWgg6jJVJ9F-YeeX8WvP5buBt0pre4zLXqcYw/pub?gid=0&single=true&output=csv"
     );
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const text = await response.text();
     const { data } = Papa.parse(text, { header: true });
+
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const shows: Show[] = data.map((row: any) => ({
       month: row.month,
